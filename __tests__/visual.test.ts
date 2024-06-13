fa975fa04342cb7f91d1cfde64a6ededc6d5b594
ryo-kozin <ryotaro.hyodo@gmail.com>
2024-06-13 15:37:27 +0900

    Set jest vial snapshots timeout globally

--- __tests__/visual.test.ts
+++ __tests__/visual.test.ts
@@ -4,12 +4,13 @@ import 'expect-puppeteer';
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
 expect.extend({ toMatchImageSnapshot });
 
+jest.setTimeout(30000);
+
 describe('Visual Regression Tests', () => {
 	beforeAll(async () => {
-		jest.setTimeout(30000);
 		await page.goto('http://localhost:3000');
 		await page.setViewport({ width: 1200, height: 800 });
-	});
+	}, 30000);
 
 	it('should visually match the snapshot', async () => {
 		const image = await page.screenshot();
@@ -17,5 +18,5 @@ describe('Visual Regression Tests', () => {
 			failureThreshold: 0.01, // 1% threshold
 			failureThresholdType: 'percent', // Use percentage threshold
 		});
-	});
+	}, 30000);
 });
