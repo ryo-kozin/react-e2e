676fb88151ebc06a5f5abd05ac4f7523ed363481
ryo-kozin <ryotaro.hyodo@gmail.com>
2024-06-13 15:09:52 +0900

    Update visual.test

--- __tests__/visual.test.ts
+++ __tests__/visual.test.ts
@@ -1,12 +1,16 @@
 import React from 'react';
+import 'expect-puppeteer';
 
 const { toMatchImageSnapshot } = require('jest-image-snapshot');
-
 expect.extend({ toMatchImageSnapshot });
 
 describe('Visual Regression Tests', () => {
-	it('should visually match the snapshot', async () => {
+	beforeAll(async () => {
 		await page.goto('http://localhost:3000');
+		await page.setViewport({ width: 1200, height: 800 });
+	});
+
+	it('should visually match the snapshot', async () => {
 		const image = await page.screenshot();
 		expect(image).toMatchImageSnapshot();
 	});
